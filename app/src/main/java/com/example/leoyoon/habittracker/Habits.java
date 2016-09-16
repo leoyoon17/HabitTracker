f34b46fb44f393ddf2ea8e47e3a68622b7f4b4f6
leoyoon17 <l.yoon17@mgmail.com>
2016-09-16 17:19:32 -0600

    Test for getting Habit tracker on Git

---
+++ app/src/main/java/com/example/leoyoon/habittracker/Habits.java
@@ -0,0 +1,13 @@
+package com.example.leoyoon.habittracker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Habits extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_habits);
+    }
+}
