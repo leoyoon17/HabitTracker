b697bd9969b9dd2162b0a854bc88a7a4840b7f26
leoyoon17 <l.yoon17@mgmail.com>
2016-09-24 17:40:19 -0600

    Added: 	In HabitList.java
    	addHabit
    	removeHabit

--- app/src/main/java/com/example/leoyoon/habittracker/HabitList.java
+++ app/src/main/java/com/example/leoyoon/habittracker/HabitList.java
@@ -16,4 +16,12 @@ public class HabitList {
     public Collection<Habit> getHabits() {
         return habitList;
     }
+
+    public void addHabit(Habit habit){
+        habitList.add(habit);
+    }
+
+    public void removeHabit(Habit habit) {
+        habitList.remove(habit);
+    }
 }
