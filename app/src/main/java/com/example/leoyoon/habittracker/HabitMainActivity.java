64d85e00e98d33d37ff89231a3ecb8ae0870e4eb
leoyoon17 <l.yoon17@mgmail.com>
2016-09-24 19:16:54 -0600

    Added: 	In HabitMainActivity.java
    	private static final String FILENAME = "file.sav";
    	    private EditText bodyText;
        	private ListView mainHabitList;
        	private ArrayList<Habit> habitList = new ArrayList<Habit>();
        	private ArrayAdapter<Habit> adapter;
    
    	mainHabitList
    
    	onStart()
    	adapter
    
    	loadFromFile()
    	ArrayList<String> habits
    
    	saveInFile()

--- app/src/main/java/com/example/leoyoon/habittracker/HabitMainActivity.java
+++ app/src/main/java/com/example/leoyoon/habittracker/HabitMainActivity.java
@@ -1,14 +1,80 @@
 package com.example.leoyoon.habittracker;
 
+import android.support.v4.widget.EdgeEffectCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.lang.reflect.Array;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
 
 public class HabitMainActivity extends AppCompatActivity {
+
+    private static final String FILENAME = "file.sav";
+    private EditText bodyText;
+    private ListView mainHabitList;
+    private ArrayList<Habit> habitList = new ArrayList<Habit>();
+    private ArrayAdapter<Habit> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_habits);
+
+        mainHabitList = (ListView) findViewById(R.id.HabitView);
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        loadFromFile();
+        adapter = new ArrayAdapter<Habit>(this, R.layout.activity_habits, habitList);
+        mainHabitList.setAdapter(adapter);
+
+    }
+
+    private void loadFromFile() {
+        ArrayList<String> habits = new ArrayList<String>();
+        try {
+            FileInputStream fis = openFileInput(FILENAME);
+            BufferedReader in = new BufferedReader(new InputStreamReader(fis));
+            Gson gson = new Gson();
+            //Code taken from http://stackoverflow.com/questions/12384064/gson-convert-from-json-to-a-typed-arraylistt Sept.22,2016
+            Type listType = new TypeToken<ArrayList<Habit>>(){}.getType();
+            habitList = gson.fromJson(in, listType );
+
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException ();
+        } catch (IOException e) {
+            throw new RuntimeException();
+        }
+    }
+
+    private void saveInFile(){
+        try {
+            FileOutputStream fos = openFileOutput(FILENAME,0);
+            OutputStreamWriter writer = new OutputStreamWriter(fos);
+            Gson gson = new Gson();
+            gson.toJson(habitList, writer);
+            writer.flush();
+        } catch (FileNotFoundException e){
+            throw new RuntimeException();
+        } catch (IOException e) {
+            throw new RuntimeException();
+        }
+    }
 }
