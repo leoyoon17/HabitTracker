7c4698daf3a1aa3e3406ac5efd9f57857e8c3955
leoyoon17 <l.yoon17@mgmail.com>
2016-09-27 20:57:16 -0600

    Issues with running application because FileNotFound Exception in loadFromFile()

--- app/src/main/java/com/example/leoyoon/habittracker/HabitMainActivity.java
+++ app/src/main/java/com/example/leoyoon/habittracker/HabitMainActivity.java
@@ -1,7 +1,6 @@
 package com.example.leoyoon.habittracker;
 
-import android.support.v4.widget.EdgeEffectCompat;
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
@@ -22,7 +21,7 @@ import java.lang.reflect.Array;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
 
-public class HabitMainActivity extends AppCompatActivity {
+public class HabitMainActivity extends Activity {
 
     private static final String FILENAME = "file.sav";
     private EditText bodyText;
@@ -42,24 +41,26 @@ public class HabitMainActivity extends AppCompatActivity {
     protected void onStart() {
         super.onStart();
         loadFromFile();
-        adapter = new ArrayAdapter<Habit>(this, R.layout.activity_habits, habitList);
+        adapter = new ArrayAdapter<Habit>(this, R.layout.list_item, habitList);
         mainHabitList.setAdapter(adapter);
 
     }
 
     private void loadFromFile() {
         ArrayList<String> habits = new ArrayList<String>();
+
         try {
             FileInputStream fis = openFileInput(FILENAME);
             BufferedReader in = new BufferedReader(new InputStreamReader(fis));
             Gson gson = new Gson();
             //Code taken from http://stackoverflow.com/questions/12384064/gson-convert-from-json-to-a-typed-arraylistt Sept.22,2016
             Type listType = new TypeToken<ArrayList<Habit>>(){}.getType();
-            habitList = gson.fromJson(in, listType );
-
+            habitList = gson.fromJson(in, listType);
         } catch (FileNotFoundException e) {
-            throw new RuntimeException ();
+            System.err.println("We can't find the file: "+FILENAME);
+            throw new RuntimeException();
         } catch (IOException e) {
+            System.err.println("Something Else?");
             throw new RuntimeException();
         }
     }
